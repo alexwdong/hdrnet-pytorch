690a4b6e2fae0f99ecf7c0c297c8645914c930f9
Alex Dong <alexwdong@gmail.com>
2021-01-08 20:55:35 -0500

    adding train.py for hdr dataset

--- scripts/train.py
+++ scripts/train.py
@@ -6,29 +6,39 @@ import math
 
 import torch
 from torchvision import transforms
-from torch.utils.data import Dataset, DataLoader,random_split
+from torch.utils.data import Dataset, DataLoader,random_split,ConcatDataset
 import pytorch_lightning as pl
 
 from pytorch_lightning.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 
+logging_path = '/home/awd275/hdrnet-pytorch/fivek_logs/'
 
-orig_photos_path = 'E:\Projects\StyleTransfer\justinho_dataset_processed\small_original_reduced_size'
-target_photos_path = 'E:\Projects\StyleTransfer\justinho_dataset_processed\small_edited_reduced_size'
+orig_photos_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/fivek_original'
+editor_A_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/fivek_editor_A'
+editor_B_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/fivek_editor_B'
+editor_C_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/fivek_editor_C'
+editor_D_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/fivek_editor_D'
+editor_E_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/fivek_editor_E'
+
+dataset_A = HDRDataset(orig_photos_path,editor_A_path)
+dataset_B = HDRDataset(orig_photos_path,editor_B_path)
+dataset_C = HDRDataset(orig_photos_path,editor_C_path)
+dataset_D = HDRDataset(orig_photos_path,editor_D_path)
+dataset_E = HDRDataset(orig_photos_path,editor_E_path)
 
 
+dataset = ConcatDataset([dataset_A,dataset_B,dataset_C,dataset_D,dataset_E])
 
-dataset = HDRDataset(orig_photos_path,target_photos_path)
+
 dataset_length = len(dataset)
 train_length = math.ceil(.8 *dataset_length)
 val_length = dataset_length-train_length
 train_set, val_set = random_split(dataset, [train_length, val_length], generator=torch.Generator().manual_seed(42))
 
-train_loader = DataLoader(train_set, batch_size = 4)
-val_loader =  DataLoader(val_set, batch_size = 4)
-
-
+train_loader = DataLoader(train_set, batch_size = 64)
+val_loader =  DataLoader(val_set, batch_size = 64)
 
 print(dataset_length)
 print(train_length)
@@ -39,7 +49,8 @@ hdrnet = HDRPointwiseNN()
 trainer = pl.Trainer(gpus=1,
                      max_epochs=500,
                      val_check_interval=.5,
-                     terminate_on_nan=True)
+                     terminate_on_nan=True,
+                     default_save_path=logging_path)
 trainer.fit(hdrnet, train_dataloader = train_loader, val_dataloaders = val_loader)
 
 
