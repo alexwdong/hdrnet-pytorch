8ba1a8d8ab22d06d1e662778387148e22cd1d2c8
alexwdong <alexwdong@gmail.com>
2021-01-25 22:56:03 -0500

    changes to train script

--- scripts/train.py
+++ scripts/train.py
@@ -13,45 +13,72 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 
-logging_path = '/home/awd275/hdrnet-pytorch/fivek_logs/'
+parser = argparse.ArgumentParser(description='take input path full of images, makes the outputpath with images at reduced size')
 
-orig_photos_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_original'
-editor_A_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_editor_A'
-editor_B_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_editor_B'
-editor_C_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_editor_C'
-editor_D_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_editor_D'
-editor_E_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_editor_E'
+parser.add_argument('-l', '--logging_path', help='logging_path',default='./')
+parser.add_argument('-g', '--gpus', help='number of gpus',default = 1)
+parser.add_argument('-m', '--mode', help='1 = hdrnet, 2 = JustinHo',default=1)
+parser.add_argument('-c', '--checkpoint_path', help='checkpoint file,if this is present, the model will load the checkpoint before training',required=False)
+parser.add_argument('-b', '--batch_size',help='batch_size, default=16', default=16)
+args = vars(parser.parse_args())
 
-dataset_A = HDRDataset(orig_photos_path,editor_A_path)
-dataset_B = HDRDataset(orig_photos_path,editor_B_path)
-dataset_C = HDRDataset(orig_photos_path,editor_C_path)
-dataset_D = HDRDataset(orig_photos_path,editor_D_path)
-dataset_E = HDRDataset(orig_photos_path,editor_E_path)
+logging_path = args['logging_path']
+mode = args['mode']
+num_gpus = args['gpus']
+checkpoint_path = args['checkpoint_path']
+batch_size=args['batch_size']
+
+if __name__ == '__main__':
+
+    if mode == 1:
+        orig_photos_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_original'
+        editor_A_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_editor_A'
+        editor_B_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_editor_B'
+        editor_C_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_editor_C'
+        editor_D_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_editor_D'
+        editor_E_path = '/scratch/awd275/StyleTransfer/data/fivek_dataset/raw/fivek_editor_E'
+
+        dataset_A = HDRDataset(orig_photos_path,editor_A_path)
+        dataset_B = HDRDataset(orig_photos_path,editor_B_path)
+        dataset_C = HDRDataset(orig_photos_path,editor_C_path)
+        dataset_D = HDRDataset(orig_photos_path,editor_D_path)
+        dataset_E = HDRDataset(orig_photos_path,editor_E_path)
 
 
-dataset = ConcatDataset([dataset_A,dataset_B,dataset_C,dataset_D,dataset_E])
+        dataset = ConcatDataset([dataset_A,dataset_B,dataset_C,dataset_D,dataset_E])
+    elif mode == 2:
+        orig_photos_path = '/scratch/awd275/StyleTransfer/data/justinho/small_original/'
+        edited_photos_path = '/scratch/awd275/StyleTransfer/data/justinho/small_edited'
+
+        dataset = HDRDataset(orig_photos_path,edited_photos_path)
+
+    else: 
+        raise ValueError('mode should be 1 or 2')
 
 
-dataset_length = len(dataset)
-train_length = math.ceil(.8 *dataset_length)
-val_length = dataset_length-train_length
-train_set, val_set = random_split(dataset, [train_length, val_length], generator=torch.Generator().manual_seed(42))
+    dataset_length = len(dataset)
+    train_length = math.ceil(.8 * dataset_length)
+    val_length = dataset_length - train_length
+    train_set, val_set = random_split(dataset, [train_length, val_length], generator=torch.Generator().manual_seed(42))
 
-train_loader = DataLoader(train_set, batch_size = 4, num_workers=24)
-val_loader =  DataLoader(val_set, batch_size = 4, num_workers=24)
+    train_loader = DataLoader(train_set, batch_size = batch_size, num_workers=24)
+    val_loader =  DataLoader(val_set, batch_size = batch_size, num_workers=24)
 
-print(dataset_length)
-print(train_length)
-print(val_length)
+    print(dataset_length)
+    print(train_length)
+    print(val_length)
 
-hdrnet = HDRPointwiseNN()
+    hdrnet = HDRPointwiseNN()
 
-trainer = pl.Trainer(gpus=1,
-                     max_epochs=50,
-                     val_check_interval=.5,
-                     terminate_on_nan=True,
-                     default_root_dir=logging_path)
-trainer.fit(hdrnet, train_dataloader = train_loader, val_dataloaders = val_loader)
+    if checkpoint_path: 
+        hdrnet.load_from_checkpoint(checkpoint_path=checkpoint_path)
+
+    trainer = pl.Trainer(gpus=num_gpus,
+                         max_epochs=30,
+                         val_check_interval=.5,
+                         terminate_on_nan=True,
+                         default_root_dir=logging_path)
+    trainer.fit(hdrnet, train_dataloader = train_loader, val_dataloaders = val_loader)
 
 
 
